f540791091d845ea73051f53396273a29e0c6924
awisu2 <awisu2@gmail.com>
2019-04-24 14:41:27 +0900

    [add] 挙動不明のためpwaの削除

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   mode: 'spa',
-  // modules: ['nuxt-sass-resources-loader', [
-  //   '~/assets/main.scss'
-  // ]],
   css: [
     { src: '~/assets/main.scss', lang: 'scss' }
-  ],
-  modules: [
-    '@nuxtjs/pwa'
-  ],
-  workbox: {
-    dev: true, // enable run on dec
-  }
+  ]
+  // modules: [
+  //   '@nuxtjs/pwa'
+  // ],
+  // workbox: {
+  //   dev: true, // enable run on dec
+  // }
 }
