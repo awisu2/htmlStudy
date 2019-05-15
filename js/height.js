f66a226b5cd1600984ee38390bc4212fead5d2d6
awisu2 <awisu2@gmail.com>
2019-05-15 15:36:07 +0900

    marginの総裁サンプルを追加

--- js/height.js
+++ js/height.js
@@ -18,8 +18,30 @@ Vue.component('height', {
         height: 10%;
       </div>
       <p class="info">
-      </p>
-  
+      </p>  
+    </section>
+
+
+    <section>
+      <h3>親子関係タグで間に要素がない場合縦方向マージンは相殺される</h3>
+      <div style="margin: 10px; background: #0F0000;">
+        <div style="margin: 50px; background: #0F0; border: solid 1px #666;">
+          margin: 10, margin: 50
+        </div>
+      </div>
+
+      <div style="margin: 10px; background: #0F0000;">
+        なにか文字を入れると相殺しない(下方向はなにもないので相殺していない)
+        <div style="margin: 50px; background: #0F0; border: solid 1px #666;">
+          margin: 10, margin: 50
+        </div>
+      </div>
+
+      <div style="margin: 10px; padding: 10px; background: #0F0000;">
+        <div style="margin: 50px; background: #0F0; border: solid 1px #666;">
+          padding をつけると相殺されない
+        </div>
+      </div>
     </section>
   </div>`,
   data: function () {
